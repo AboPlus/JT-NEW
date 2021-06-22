62037eeba2e2b80411c403ff2865bf09fdff828d
Abo <252841200@qq.com>
2021-06-22 14:30:46 +0800

    分布式项目结构init

---
+++ 01-jt/jt-portal/jt-member/src/main/java/com/cy/jt/MemberApplication.java
@@ -0,0 +1,14 @@
+package com.cy.jt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Abo
+ */
+@SpringBootApplication
+public class MemberApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(MemberApplication.class, args);
+    }
+}
