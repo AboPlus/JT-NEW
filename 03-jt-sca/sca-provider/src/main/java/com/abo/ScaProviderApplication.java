7a1475e535eef10d8e296557a0516ff2f8963318
Abo <252841200@qq.com>
2021-06-25 14:52:36 +0800

    do study nacos

---
+++ 03-jt-sca/sca-provider/src/main/java/com/abo/ScaProviderApplication.java
@@ -0,0 +1,14 @@
+package com.abo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Abo
+ */
+@SpringBootApplication
+public class ScaProviderApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ScaProviderApplication.class, args);
+    }
+}
