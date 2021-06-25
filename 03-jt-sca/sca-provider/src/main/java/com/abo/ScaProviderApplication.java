d8e8e0d5c96442dd7ca6ec252c13e5c1e6347047
Abo <252841200@qq.com>
2021-06-25 15:34:41 +0800

    do study nacos

--- 03-jt-sca/sca-provider/src/main/java/com/abo/ScaProviderApplication.java
+++ 03-jt-sca/sca-provider/src/main/java/com/abo/ScaProviderApplication.java
@@ -1,7 +1,11 @@
 package com.abo;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
 
 /**
  * @author Abo
@@ -11,4 +15,16 @@ public class ScaProviderApplication {
     public static void main(String[] args) {
         SpringApplication.run(ScaProviderApplication.class, args);
     }
+
+    @Value("${server.port}")
+    private String server;
+
+    @RestController
+    public class ProviderController {
+        @GetMapping("/provider/echo/{msg}")
+        public String doEcho(@PathVariable String msg){
+            return server + "say:Hello Nacos Discovery " + msg;
+        }
+    }
+
 }
